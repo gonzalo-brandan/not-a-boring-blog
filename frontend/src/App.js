9a42940bf189f0e89fe52ec5d92c5ef274c1ee58
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-07 15:20:33 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import Blog from './components/Blog';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
