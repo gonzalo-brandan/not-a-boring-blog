bdb1702f8931f24f66722a831eac9acb8a65096d
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-14 10:51:39 +0200

    add postPage page

---
+++ frontend/src/components/PostPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import AppBar from './AppBar'
+
+export default class PostPage extends Component {
+  
+  render() {
+    return (
+      
+      <div>
+        <AppBar />
+      </div>
+    )
+  }
+}
