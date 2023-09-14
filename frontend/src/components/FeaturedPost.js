15b09c03da980b512ec07fbd8c1aae883ffc7235
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-14 12:56:45 +0200

    update featuredPost styling. Bigger layout.

--- frontend/src/components/FeaturedPost.js
+++ frontend/src/components/FeaturedPost.js
@@ -1,43 +1,88 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+
 
 function FeaturedPost(props) {
   const { post } = props;
 
   return (
-    <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href="#">
-        <Card sx={{ display: 'flex' }}>
-          <CardContent sx={{ flex: 1 }}>
-            <Typography component="h2" variant="h5">
-              {post.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {post.date}
-            </Typography>
-            <Typography variant="subtitle1" paragraph>
-              {post.description}
-            </Typography>
-            <Typography variant="subtitle1" color="primary">
-              Continue reading...
-            </Typography>
-          </CardContent>
-          <CardMedia
-            component="img"
-            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-            image={post.image}
-            alt={post.imageLabel}
-          />
-        </Card>
-      </CardActionArea>
+    <Paper
+  sx={{
+    position: 'relative',
+    backgroundColor: 'grey.800',
+    color: '#fff',
+    mb: 4,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundImage: `url(${props.image})`,
+  }}
+>
+  {/* Increase the priority of the hero background image */}
+  {<img style={{ display: 'none' }} src={post.image} alt={post.imageText} />}
+  <Box
+    sx={{
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      backgroundColor: 'rgba(0,0,0,.3)',
+    }}
+  />
+  <Grid container>
+    <Grid item md={6}>
+      <Box
+        sx={{
+          position: 'relative',
+          p: { xs: 3, md: 6 },
+          pr: { md: 0 },
+        }}
+      >
+        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+          {post.title}
+        </Typography>
+        <Typography variant="h5" color="inherit" paragraph>
+          {post.description}
+        </Typography>
+        <Link variant="subtitle1" href="#">
+          {props.linkText}
+        </Link>
+      </Box>
+      </Grid>
+      <Grid item md={6}>
+          <Grid container justifyContent="flex-end">
+      <Box
+        sx={{
+          position: 'relative',
+          backgroundColor: 'rgba(0, 0, 0, 0.7)', // 50%
+          borderRadius: '25%',
+          p: 2,
+          mt: 2,
+          mr: 4,
+          display: 'flex',
+          p: { xs: 3, md: 6 },
+          pr: { md: 0 },
+        }}
+      >       
+        <Link variant="subtitle1" href="#">
+          {post.category}
+        </Link>
+        <Link variant="subtitle1" href="#">
+          {post.category}
+        </Link>
+
+      </Box>
     </Grid>
-  );
+  </Grid>
+  </Grid>
+</Paper>
+);
 }
 
 FeaturedPost.propTypes = {
