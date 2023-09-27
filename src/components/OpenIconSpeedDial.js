4435744a4a1cfed28e1e0aed63c44c0486a2a15c
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-27 12:52:22 +0200

    create OpenIconSpeedDial for shortcut for creating a post

---
+++ src/components/OpenIconSpeedDial.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import EditIcon from '@mui/icons-material/Edit';
+
+const actions = [
+  { icon: <FileCopyIcon />, name: 'Create New Post', href: '/create_post' },
+];
+
+export default function OpenIconSpeedDial() {
+  return (
+    <Box sx={{ position: 'fixed', bottom: 16, right: 16, zIndex: 1000 }}>
+      <SpeedDial
+        ariaLabel="SpeedDial openIcon example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon openIcon={<EditIcon />} />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            href={action.href}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
+  );
+}
