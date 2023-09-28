f50a7b17b9bcfdd409ef5dc2363a87eb4d68f97c
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-28 12:19:42 +0200

    make icon bigger for big screens and redirect to /create_post when clicked

--- src/components/OpenIconSpeedDial.js
+++ src/components/OpenIconSpeedDial.js
@@ -4,32 +4,43 @@ import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
 import EditIcon from '@mui/icons-material/Edit';
+import { useMediaQuery } from '@mui/material';
+import Link from '@mui/material/Link';
 
 const actions = [
-  { icon: <FileCopyIcon />, name: 'Create New Post', href: '/create_post' },
+  { icon: <FileCopyIcon sx={{ fontSize: 30 }}/>, name: 'Create New Post', href: '/create_post' },
 ];
 
 export default function OpenIconSpeedDial() {
+  const isBigScreen = useMediaQuery('(min-width: 768px)'); 
   return (
     <Box sx={{ position: 'fixed', bottom: 16, right: 16, zIndex: 1000 }}>
-      <SpeedDial
+      <Link href="/create_post">
+        <SpeedDial
         ariaLabel="SpeedDial openIcon example"
-        sx={{ position: 'absolute', bottom: 16, right: 16 }}
-        icon={<SpeedDialIcon openIcon={<EditIcon />} />}
+        sx={{
+          position: 'absolute',
+          bottom: 16,
+          right: 16,
+          ...(isBigScreen && {
+            '& .MuiFab-primary': { width: 80, height: 80 },
+          }),
+          
+        }}
+        href={'/create_post/'}
+        icon={<SpeedDialIcon />}
       >
-        {actions.map((action) => (
+        {/* {actions.map((action) => (
           <SpeedDialAction
             key={action.name}
             icon={action.icon}
             tooltipTitle={action.name}
             href={action.href}
           />
-        ))}
+        ))} */}
       </SpeedDial>
+      </Link>
     </Box>
   );
 }
