75598d42ec4bd2b4a470c466382d1ed6e5c3e817
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-09-28 10:36:47 +0200

    use navigate to '/' after user created a new post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,26 +1,18 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AppBar from '../components/AppBar'
-import { fetchUsers } from '../components/fetchUsers';
 import { useState, useEffect } from 'react';
 import Footer from '../components/Footer'
-
-import MultilineTextFields from '../components/TextInput';
 import { TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -31,6 +23,8 @@ const categories = [
   ];
 
   export default function CreatePost() {
+
+    const navigate = useNavigate();
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
@@ -76,6 +70,7 @@ const categories = [
         } catch (error) {
           console.error('Error creating post:', error);
         }
+        navigate('/')
       };
 
   return (
