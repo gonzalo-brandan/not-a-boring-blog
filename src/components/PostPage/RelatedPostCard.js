31b0a40e04e99760bd6d20ce09d6e0cce118785d
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-10-03 18:04:17 +0200

    add border, boxShadow and 'posted by' to card.

--- src/components/PostPage/RelatedPostCard.js
+++ src/components/PostPage/RelatedPostCard.js
@@ -3,6 +3,10 @@ import Avatar from '@mui/material/Avatar';
 import { useNavigate } from 'react-router-dom';
 import AuthService from '../../Auth/AuthService';
 import { useState, useEffect } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const defaultTheme = createTheme();
 
 function RelatedPostCard(props) {
     const { title, author } = props;
@@ -18,16 +22,22 @@ function RelatedPostCard(props) {
         navigate('/login');
       }
     };
-    console.log(post)
+
     return(
-<CardActionArea sx={{ p: 2, mt: 2}} onClick={handleClick}>
-<List sx={{ py: 0}}>
+<CardActionArea sx={{ 
+  p: 2, 
+  mt: 2,   
+  border: `1px solid ${defaultTheme.palette.primary.main}`,
+  borderRadius: 4,
+  boxShadow: `0 0 5px 0 ${defaultTheme.palette.primary.main}`,}} 
+  onClick={handleClick}>
+  <List sx={{ py: 0}}>
   {/* Placeholder text */}
   <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
     <ListItemText
       sx={{ py: 0 }}
       primary={title}
-      secondary={author}
+      secondary={`Posted by ${author}`}
     />
   </ListItem>
 </List>
