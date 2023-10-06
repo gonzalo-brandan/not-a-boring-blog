d857bffcc174c962c9afe06852afaa35008b08de
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-10-06 16:02:04 +0200

    create PasswordField component

---
+++ src/components/PasswordField.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+const PasswordField = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <TextField
+      margin="normal"
+      required
+      fullWidth
+      name={props.name}
+      label={props.label}
+      type={showPassword ? 'text' : 'password'}
+      id={props.id}
+      autoComplete={props.autoComplete}
+      value={props.value}
+      onChange={props.onChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
+
+export default PasswordField;
