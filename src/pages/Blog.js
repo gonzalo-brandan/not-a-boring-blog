ed085b1d6437ed18d19a3a20db4c56b46a8bb9b6
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-10-07 14:22:04 +0200

    remove old sections and user authentication (not used)

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -5,42 +5,17 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from '../components/Header';
 import FeaturedPost from '../components/FeaturedPost';
-import post1 from '../Assets/Posts/blog-post.1.md';
-import post2 from '../Assets/Posts/blog-post.2.md';
-import post3 from '../Assets/Posts/blog-post.3.md';
-
-
 import { useState, useEffect } from 'react';
-import AuthService from '../components/AuthService';import { fetchPosts } from '../components/fetchPosts';
-
+import { fetchPosts } from '../components/fetchPosts';
 import OpenIconSpeedDial from '../components/OpenIconSpeedDial'
-import { SpeedDial } from '@mui/material';
-
-const sections = [
-  { title: 'Technology', url: '#' },
-  { title: 'Design', url: '#' },
-  { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
-  { title: 'Politics', url: '#' },
-  { title: 'Opinion', url: '#' },
-  { title: 'Science', url: '#' },
-  { title: 'Health', url: '#' },
-  { title: 'Style', url: '#' },
-  { title: 'Travel', url: '#' },
-];
-
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function Blog() {
-
-  const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser);
-
   const [posts1, setPosts1] = useState([]);
 
   useEffect(() => {
-    // Fetch posts when the component mounts
     fetchPosts()
       .then((response) => {
         setPosts1(response.data);
@@ -55,7 +30,7 @@ export default function Blog() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Not Boring Blog" sections={sections} />
+        <Header title="Not Boring Blog" />
         <main>
           <Grid sx={{mt: 1}} container spacing={4}>
             {posts1.map((posts1) => (
@@ -65,7 +40,7 @@ export default function Blog() {
           </Grid>
         </main>
       </Container>
-            <OpenIconSpeedDial />
+      <OpenIconSpeedDial />
     </ThemeProvider>
   );
 }
