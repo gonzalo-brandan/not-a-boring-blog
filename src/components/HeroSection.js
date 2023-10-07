949763ae96396acda17043ff6008ce4861e4d742
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-10-07 15:49:55 +0200

    create HeroSection component

---
+++ src/components/HeroSection.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+const HeroSection = ({ title, description, primaryButtonText, secondaryButtonText }) => {
+  return (
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        pt: 8,
+        pb: 6,
+      }}
+    >
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          {title}
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+          {description}
+        </Typography>
+        <Stack
+          sx={{ pt: 4 }}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+        >
+          <Button variant="contained" color="primary">
+            {primaryButtonText}
+          </Button>
+          <Button variant="outlined" color="secondary">
+            {secondaryButtonText}
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default HeroSection;
