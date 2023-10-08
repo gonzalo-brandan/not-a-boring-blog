71f76b9fbb38b908cc1c37a6b8780be72576be5a
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-10-08 16:48:33 +0200

    fix comment deletion logic and dialog handling to prevent infinite re-renders

--- src/components/CommentsSection.js
+++ src/components/CommentsSection.js
@@ -3,42 +3,30 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-
-import ReplyIcon from '@mui/icons-material/Reply';
-import CommentIcon from '@mui/icons-material/Comment';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-
-import DeleteCommentIcon from './DeleteCommentIcon';
-
-import { List, Card, ListItem, ListItemText, ListItemAvatar, IconButton } from '@mui/material';
+import { List, Card, ListItem, ListItemText, ListItemAvatar, IconButton, DialogTitle, DialogContent, DialogContentText, DialogActions, Dialog } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import AuthService from '../components/AuthService';
 import { useParams } from 'react-router-dom';
-import { fetchComments } from './fetchComments';
 import axios from 'axios';
 import DeleteIcon from '@mui/icons-material/Delete';
-
-
+import DeleteCommentButton from './DeleteCommentButton';
 
 function CommentsSection(props) {
-
-  const [replyText, setReplyText] = useState('');
-  const [isReplying, setIsReplying] = useState(false); // State to track whether the user is replying
-
-  const handleReplySubmit = () => {
-    setReplyText('');
-
-    if (props.onReply) {
-      props.onReply(replyText);
-    }
-
-    setIsReplying(false);
-  };
-  
   const [newComment, setNewComment] = useState('');
   const [comments, setComments] = useState([]);
   const { postId } = useParams();
-  const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser())
+  const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
+  const [open, setOpen] = useState(false);
+  const [commentIdToDelete, setCommentIdToDelete] = useState(null); // State to store comment ID to delete
+
+  const handleClickOpen = (commentId) => {
+    setCommentIdToDelete(commentId); // Set the comment ID to delete
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     const getCurrentUser = async () => {
@@ -48,16 +36,17 @@ function CommentsSection(props) {
 
     getCurrentUser();
   }, []);
-  
-useEffect(() => {
+
+  useEffect(() => {
     axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}comment/comments/${postId}/`)
       .then(response => {
-        setComments(response.data); 
+        setComments(response.data);
       })
       .catch(error => {
         console.error('Error fetching post data:', error);
       });
-  }, [postId]); 
+  }, [postId]);
+
   const fetchAndUpdateComments = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}comment/comments/${postId}/`);
@@ -68,7 +57,7 @@ useEffect(() => {
       console.error('Error fetching comments:', error);
     }
   };
-    
+
   const handleCommentSubmit = async () => {
     const storedToken = localStorage.getItem('token');
     try {
@@ -78,7 +67,7 @@ useEffect(() => {
           'Content-Type': 'application/json',
           'Authorization': `Token ${storedToken}`
         },
-        body: JSON.stringify({ postId, body: newComment, author_username: 'mod'}),
+        body: JSON.stringify({ postId, body: newComment, author_username: 'mod' }),
       });
 
       if (response.ok) {
@@ -93,10 +82,10 @@ useEffect(() => {
     }
   };
 
-  const handleCommentDeletion = async (commentId) => {
+  const handleCommentDeletion = async () => {
     const storedToken = localStorage.getItem('token');
     try {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}comment/update_comment/${commentId}/`, {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}comment/update_comment/${commentIdToDelete}/`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -105,8 +94,9 @@ useEffect(() => {
       });
 
       if (response.ok) {
-        const updatedComments = comments.filter((comment) => comment.id !== commentId);
+        const updatedComments = comments.filter((comment) => comment.id !== commentIdToDelete);
         setComments(updatedComments);
+        handleClose();
       } else {
         console.error('Error deleting comment');
       }
@@ -116,9 +106,7 @@ useEffect(() => {
   };
 
   return (
-    
     <div>
-      
       <Paper elevation={0} sx={{ p: 2, bgcolor: 'grey.200', mt: 3 }}>
         <Typography variant="h6" gutterBottom>
           Comments
@@ -132,100 +120,78 @@ useEffect(() => {
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
           sx={{ mt: 2, background: 'white' }}
-          />
+        />
         <Button
           variant="contained"
           color="primary"
           onClick={handleCommentSubmit}
           sx={{ mt: 2 }}
-          >
-          Post Comment
-        </Button>        
-        {comments && comments.map((comment, index) => (
-            <Card sx={{ p: 2, mt: 3, mb: 3 }}>
-              <List sx={{ py: 0 }}>
-                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                  <ListItemAvatar>
-                    <Avatar alt="User" src="/user-avatar.jpg" />
-                  </ListItemAvatar>
-                  <ListItemText
-                    sx={{ py: 0 }}
-                    primary={comment.author_username}
-                    secondary={comment.body}
-                  />
-                  
-                </ListItem>
-                {/* <IconButton aria-label="reply">
-                  <ReplyIcon />
-                </IconButton>
-                <IconButton aria-label="comments">
-                  <CommentIcon />
-                </IconButton> */}
-                {localStorage.username === comment.author_username && (
-                <IconButton 
-                aria-label="delete"
-                sx={{
-                  position: 'absolute',
-                  top: 0,
-                  right: 0,
-                }}
-                onClick={() => handleCommentDeletion(comment.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-                )}
-            {/* REPLY */}
-            {/* {isReplying ? ( // Render the input field if isReplying is true
-            <div>
-            <TextField
-            label="Reply to comment"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={4}
-            value={replyText}
-            onChange={(e) => setReplyText(e.target.value)}
-            sx={{ mt: 2 }}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleReplySubmit}
-            sx={{ mt: 2 }}
-          >
-            Reply
-          </Button>
-        </div>
-      ) : (
-        // Render the "Reply" button if isReplying is false
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={() => setIsReplying(true)} 
-          sx={{ mt: 2 }}
         >
-          Reply
+          Post Comment
         </Button>
-      )} */}
-        <List>
-        {comment.replies && comment.replies.map((reply) => (
-            <ListItem key={reply.id} alignItems="center" disableGutters>
-              <ListItemAvatar>
-                <Avatar alt="User" src="/user-avatar.jpg" />
-              </ListItemAvatar>
-              <ListItemText
-                primary={reply.author_username}
-                secondary={reply.body}
-              />
-              {/* You can add more buttons or actions for each reply here */}
-            </ListItem>
-          ))}
-        </List>
-    
-    </List>
-  </Card>
-))}
-          
+        {comments && comments.map((comment, index) => (
+          <Card sx={{ p: 2, mt: 3, mb: 3 }}>
+            <List sx={{ py: 0 }}>
+              <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
+                <ListItemAvatar>
+                  <Avatar alt="User" src="/user-avatar.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  sx={{ py: 0 }}
+                  primary={comment.author_username}
+                  secondary={comment.body}
+                />
+              </ListItem>
+              {localStorage.username === comment.author_username && (
+                <div>
+                  <IconButton
+                    aria-label="delete"
+                    sx={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 0,
+                    }}
+                    onClick={() => handleClickOpen(comment.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                  <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                  >
+                    <DialogTitle id="alert-dialog-title">
+                      {"Are you sure you want to delete this post?"}
+                    </DialogTitle>
+                    <DialogContent>
+                      <DialogContentText id="alert-dialog-description">
+                        This is an irreversible action.
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+                      <Button variant="outlined" color="error" onClick={handleCommentDeletion} autoFocus>Delete</Button>
+                    </DialogActions>
+                  </Dialog>
+                </div>
+              )}
+              <List>
+                {comment.replies && comment.replies.map((reply) => (
+                  <ListItem key={reply.id} alignItems="center" disableGutters>
+                    <ListItemAvatar>
+                      <Avatar alt="User" src="/user-avatar.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={reply.author_username}
+                      secondary={reply.body}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </List>
+          </Card>
+        ))}
       </Paper>
     </div>
   );
