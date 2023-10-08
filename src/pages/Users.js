132c012adea1da84d2c3ed3b878642536d46b42e
gonzalo-brandan <gonzalobrandan@outlook.de>
2023-10-08 17:04:46 +0200

    use HeroSection component

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -16,7 +16,7 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { fetchUsers } from '../components/fetchUsers';
 import { useState, useEffect } from 'react';
-
+import HeroSection from '../components/HeroSection'
 
 function Copyright() {
   return (
@@ -58,40 +58,7 @@ export default function Users() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Users layout
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
-          </Container>
-        </Box>
+      <HeroSection title='Explore Our Community' description='Meet our diverse community of users'/>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
